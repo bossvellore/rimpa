66235d09a75b9f22aaf07d54476211d3e3991a16
amalroshand <velloreamal@gmail.com>
2017-05-15 17:30:31 +0530

    Merge branch 'master' of https://github.com/bossvellore/rimpa
    # Conflicts:
    #	src/main/java/com/dsa/rimpark/MainActivity.java

--- src/main/java/com/dsa/rimpark/MainActivity.java
+++ src/main/java/com/dsa/rimpark/MainActivity.java
@@ -286,10 +286,9 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.logoutMenu) {
-
-            return true;
-        }
+        //if (id == R.id.logoutMenu) {
+        //  return true;
+        //}
         return super.onOptionsItemSelected(item);
     }
 
